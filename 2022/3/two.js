6286e5374929d0bef72681437b1cb67220926fdf
Tom Naumann <tom.naumann@bayer.com>
2022-12-05 10:19:00 +0100

    2022 Day 3 - Part 2

---
+++ 2022/3/two.js
@@ -0,0 +1,28 @@
+import { readFileSync } from "fs";
+
+const contents = readFileSync("2022/3/data.txt", "utf-8");
+const arr = contents.split(/\r?\n/);
+
+function score(c) {
+    let s = 0;
+    if (c === c.toUpperCase()) {
+        s = 26;
+        c = c.toLowerCase();
+    }
+    s += c.charCodeAt(0) - 96;
+    return s;
+}
+
+let scores = [];
+for (let i = 0; i < arr.length; i+=3) {
+    let s = arr[i];
+    for (let sx = 0; sx < s.length; sx++) {
+        let c = s.charAt(sx)
+        if (arr[i+1].includes(c) && arr[i+2].includes(c)) {
+            scores.push(score(c));
+            break;
+        }
+    }
+}
+
+console.log(scores.reduce((partialSum, a) => partialSum + a, 0));
